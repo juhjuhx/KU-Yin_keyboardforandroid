a9c5dba7ee7fb84e6baada3c338344232a19d2cb
juhjuhx <60009071+juhjuhx@users.noreply.github.com>
2026-09-09 17:29:25 +0800

    test: add JVM unit test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.0")
+
+    testImplementation("junit:junit:4.13.2")
 }
